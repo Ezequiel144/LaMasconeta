c205fe551261508da467692a9cba72ad3463a36a
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-04-16 19:33:13 -0300

    usando la dependencia de swiper

---
+++ src/components/ui/CarouselHero/CarouselHero.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Image from "next/image";
+/* import "swiper/swiper.min.css"; */
+
+export default function CarouselHero() {
+  return (
+    <Swiper className=" w-full h-[580px]">
+      <SwiperSlide >
+        <div className=" flex flex-row items-center justify-between">
+          <article className="flex flex-col gap-y-8 pl-20">
+            <h4 className=" text-violetGrow-700 font-bold text-xl">
+              lorem lorem lorem lorem
+            </h4>
+            <h1 className=" text-violetGrow-700 font-bold text-5xl">
+              Adopcion lorem lorem
+            </h1>
+            <button className=" w-fit text-violetGrow-700 text-xl font-semibold uppercase px-[15px] py-[10px] rounded-lg border-2 border-violetGrow-700">
+              adoptar ahora
+            </button>
+          </article>
+          <Image
+            src={"/image/HeroSlide1.png"}
+            width={588}
+            height={588}
+            alt="slide-1"
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>slide 2</SwiperSlide>
+    </Swiper>
+  );
+}
