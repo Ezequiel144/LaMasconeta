aa1536db061fc34c10ba15c8f63eca9a98b669fd
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-04-17 19:04:10 -0300

    haciendo el interface y la lista

--- src/app/(pet)/page.tsx
+++ src/app/(pet)/page.tsx
@@ -1,6 +1,39 @@
 import CarouselHero from "@/components/ui/CarouselHero/CarouselHero";
 import HeroSectionTwo from "@/components/ui/HeroSectionTwo/HeroSectionTwo";
 import TitleGeneralHome from "@/components/ui/TitleGeneralHome/TitleGeneralHome";
+import { objectCardPets } from "@/interfaces";
+import Image from "next/image";
+
+const listCardPets: objectCardPets[] = [
+  {
+    name: "nombre del perro",
+    province: "province",
+    location: "localidad",
+    typeOfAnimal: "tipo de animal",
+    image: "/image/imagePet.png",
+  },
+  {
+    name: "nombre del perro",
+    province: "province",
+    location: "localidad",
+    typeOfAnimal: "tipo de animal",
+    image: "/image/imagePet.png",
+  },
+  {
+    name: "nombre del perro",
+    province: "province",
+    location: "localidad",
+    typeOfAnimal: "tipo de animal",
+    image: "/image/imagePet.png",
+  },
+  {
+    name: "nombre del perro",
+    province: "provincia",
+    location: "localidad",
+    typeOfAnimal: "tipo de animal",
+    image: "/image/imagePet.png",
+  },
+];
 
 export default function Home() {
   return (
@@ -11,7 +44,15 @@ export default function Home() {
         <article className="w-full text-right">
           <TitleGeneralHome>title lorem lorem</TitleGeneralHome>
         </article>
-        <article></article>
+        <article className=" flex ">
+          {listCardPets.map((item,index) => {
+            return (
+              <div key={index} className="border h-[330px] w-[280px] rounded-xl">
+                <Image src={item.image} width={280} height={255} alt={item.name} />
+              </div>
+            );
+          })}
+        </article>
       </section>
     </main>
   );
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,2 +1,3 @@
 export * from "./listMenu.interface"
 export * from "./listInfoNumber.interface"
+export * from "./listCardPets.interface"
