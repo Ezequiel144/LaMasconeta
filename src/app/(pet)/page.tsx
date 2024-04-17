6c5226836025c87ade46e3a5542c0eb416d8f210
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-04-16 23:34:51 -0300

    carrusel responsive

--- src/app/(pet)/page.tsx
+++ src/app/(pet)/page.tsx
@@ -1,9 +1,11 @@
 import CarouselHero from "@/components/ui/CarouselHero/CarouselHero";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col ">
       <CarouselHero />
+      {/* <img src={"/image/ImageSection2Numbers.png"} className="w-full h-fit pt-10" alt="imagenBackgroundAdopcion"/> */}
     </main>
   );
 }
--- src/components/ui/CarouselHero/CarouselHero.tsx
+++ src/components/ui/CarouselHero/CarouselHero.tsx
@@ -27,8 +27,9 @@ export default function CarouselHero() {
             <h1 className=" text-violetGrow-700 font-bold text-4xl lg:text-5xl text-center lg:text-left">
               Adopcion lorem lorem
             </h1>
-            <div className=" w-full flex justify-center lg:justify-start"><ButtonGeneral> Adoptar Ahora </ButtonGeneral></div>
-            
+            <div className=" w-full flex justify-center lg:justify-start">
+              <ButtonGeneral> Adoptar Ahora </ButtonGeneral>
+            </div>
           </article>
           <Image
             src={"/image/HeroSlide1.png"}
@@ -40,25 +41,31 @@ export default function CarouselHero() {
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className=" flex flex-row items-center justify-between">
-          <article className="flex flex-col gap-y-8 pl-20">
-            <h4 className=" text-violetGrow-700 font-bold text-xl">
+        <div className=" flex flex-col-reverse lg:flex-row items-center justify-between gap-y-3">
+          <article className="flex flex-col gap-y-8 lg:pl-20">
+            <h4 className=" text-violetGrow-700 font-bold text-base lg:text-xl text-center lg:text-left">
               lorem lorem lorem lorem
             </h4>
-            <h1 className=" text-violetGrow-700 font-bold text-5xl">
+            <h1 className=" text-violetGrow-700 font-bold text-4xl lg:text-5xl text-center lg:text-left">
               Adopcion lorem lorem
             </h1>
-            <ButtonGeneral> Adoptar Ahora </ButtonGeneral>
+            <div className=" w-full flex justify-center lg:justify-start">
+              <ButtonGeneral> Adoptar Ahora </ButtonGeneral>
+            </div>
           </article>
           <Image
             src={"/image/HeroSlide1.png"}
             width={588}
             height={588}
+            className=" w-full md:w-[588px] md:h-[588px]"
             alt="slide-1"
           />
         </div>
       </SwiperSlide>
-      <button className="absolute top-1/2 left-10 z-10 cursor-pointer" id="arrowPrev">
+      <button
+        className="absolute top-1/2 left-10 z-10 cursor-pointer"
+        id="arrowPrev"
+      >
         <Image
           src={"../ArrowLeft.svg"}
           width={13}
@@ -66,7 +73,10 @@ export default function CarouselHero() {
           alt="arrowLeft "
         />
       </button>
-      <button className="absolute top-1/2 right-10 z-10 cursor-pointer" id="arrowNext">
+      <button
+        className="absolute top-1/2 right-10 z-10 cursor-pointer"
+        id="arrowNext"
+      >
         <Image
           src={"../ArrowRight.svg"}
           width={13}
