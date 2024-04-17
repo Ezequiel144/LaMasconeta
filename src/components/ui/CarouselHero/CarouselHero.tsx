29913cfa99774eb2a7d28dbc5ef10665a3f9da91
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-04-16 21:14:11 -0300

    terminando el carrusel falta los slides

--- src/components/ui/CarouselHero/CarouselHero.tsx
+++ src/components/ui/CarouselHero/CarouselHero.tsx
@@ -7,8 +7,8 @@ import ButtonGeneral from "../ButtonGeneral/ButtonGeneral";
 
 export default function CarouselHero() {
   return (
-    <Swiper className=" w-full h-[580px]">
-      <SwiperSlide >
+    <Swiper className=" w-full h-[580px] relative">
+      <SwiperSlide>
         <div className=" flex flex-row items-center justify-between">
           <article className="flex flex-col gap-y-8 pl-20">
             <h4 className=" text-violetGrow-700 font-bold text-xl">
@@ -27,7 +27,31 @@ export default function CarouselHero() {
           />
         </div>
       </SwiperSlide>
-      <SwiperSlide>slide 2</SwiperSlide>
+      <SwiperSlide>
+        <div className=" flex flex-row items-center justify-between">
+          <article className="flex flex-col gap-y-8 pl-20">
+            <h4 className=" text-violetGrow-700 font-bold text-xl">
+              lorem lorem lorem lorem
+            </h4>
+            <h1 className=" text-violetGrow-700 font-bold text-5xl">
+              Adopcion lorem lorem
+            </h1>
+            <ButtonGeneral> Adoptar Ahora </ButtonGeneral>
+          </article>
+          <Image
+            src={"/image/HeroSlide1.png"}
+            width={588}
+            height={588}
+            alt="slide-1"
+          />
+        </div>
+      </SwiperSlide>
+      <button className="absolute top-1/2 left-10 z-10">
+        <Image src={"../ArrowLeft.svg"} width={13} height={24} alt="arrowLeft " />
+      </button>
+      <button className="absolute top-1/2 right-10 z-10">
+        <Image src={"../ArrowRight.svg"} width={13} height={24} alt="arrowRight"/>
+      </button>
     </Swiper>
   );
 }
