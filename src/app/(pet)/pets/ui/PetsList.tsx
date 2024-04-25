b4afd61c7b2f0f42dff7919e97fe434a441958df
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-04-25 15:42:21 -0300

    termiando el paginado

--- src/app/(pet)/pets/ui/PetsList.tsx
+++ src/app/(pet)/pets/ui/PetsList.tsx
@@ -2,7 +2,7 @@ import { agePets, getProvinces, getSpecies } from "@/actions";
 import { getPetByParams } from "@/actions/pets/get-pet-by-params";
 import { FilterParams } from "@/interfaces";
 import FilterContain from "./FilterContain/FilterContain";
-import CardsPets from "@/components/ui/CardPets/CardPets";
+import RenderCardAndPagination from "./RenderCardAndPagination/RenderCardAndPagination";
 
 export const PetsList = async ({ searchParams }: FilterParams) => {
   const pets = await getPetByParams({ searchParams });
@@ -17,24 +17,7 @@ export const PetsList = async ({ searchParams }: FilterParams) => {
         petsProvinces={petsProvinces}
         petsAges={petsAges}
       />
-      {/* pets.map((pet) => (
-        <pre key={pet.id}>{JSON.stringify(pet,null,2)}</pre>
-      )) */}
-      <section className=" grid grid-cols-gridResponsiveFilter justify-items-center gap-10 w-full pt-20 lg:px-16">
-        {pets.map((item, index) => {
-          const {name, slug, species, province, photos} = item;
-          return (
-            <CardsPets
-              key={index}
-              image={photos[0]}
-              name={name}
-              province={province.name}
-              typeOfAnimal={species.name}
-              slug={slug}
-            />
-          );
-        })}
-      </section>
+      <RenderCardAndPagination pets={pets}/>
     </div>
   );
 };
