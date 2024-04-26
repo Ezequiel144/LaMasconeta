ac5c32c1ca93f938d7d4009e055d387be8ac3c0f
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-04-26 15:09:10 -0300

    modificando un loading

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -16,7 +16,7 @@ export default async function AdminLayout({
   return (
     <div className="w-full max-w-7xl mx-auto min-h-screen px-6 py-2 flex gap-y-10 gap-x-32 lg:gap-x-28 flex-col md:flex-row">
       <SidebarProfile />
-      <div className="w-full md:w-8/12 bg-[#F8F8F8] rounded-xl p-5">{children}</div>
+      <div className="w-full md:w-8/12 bg-[#F8F8F8] rounded-xl p-5 h-fit">{children}</div>
     </div>
   );
 }
