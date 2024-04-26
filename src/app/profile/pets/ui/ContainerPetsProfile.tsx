d79ded0a42236566f41f24996ac1169b483b498c
Mauri <mauri.monzon91@gmail.com>
2024-04-26 12:13:12 -0300

    Todos las publicaciones del usuerio

--- src/app/profile/pets/ui/ContainerPetsProfile.tsx
+++ src/app/profile/pets/ui/ContainerPetsProfile.tsx
@@ -1,7 +1,44 @@
+import CardPetsProfile from "./CardPetsProfile";
 
+interface PetData {
+  id: string;
+  name: string;
+  slug: string;
+  statusAdoption: string | null;
+  photos: string[];
+  province: {
+    id: string;
+    name: string;
+    slug: string;
+  };
+  species: {
+    id: string;
+    name: string;
+  };
+}
 
-export const ContainerPetsProfile = ({ pets }: PetData) => {
-  console.log(pets);
+interface ContainerPetsProfileProps {
+  pets: PetData[] | { ok: boolean; message: string };
+}
 
-  return <div>ContainerPetsProfile</div>;
+export const ContainerPetsProfile = ({ pets }: ContainerPetsProfileProps) => {
+  if ("ok" in pets && "message" in pets) {
+    return <div>Error: {pets.message}</div>;
+  }
+
+  return (
+    <div className="grid grid-cols-3 gap-6">
+      {pets.map((pet: PetData) => (
+        <CardPetsProfile
+          key={pet.slug}
+          name={pet.name}
+          slug={pet.slug}
+          statusAdoption={pet.statusAdoption}
+          photos={pet.photos}
+          province={pet.province}
+          species={pet.species}
+        />
+      ))}
+    </div>
+  );
 };
