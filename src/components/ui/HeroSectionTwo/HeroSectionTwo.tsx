b51bd76860070257151c438e8476f474997d30c3
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-04-26 15:27:32 -0300

    arraglando la home y el responsive

--- src/components/ui/HeroSectionTwo/HeroSectionTwo.tsx
+++ src/components/ui/HeroSectionTwo/HeroSectionTwo.tsx
@@ -7,6 +7,8 @@ import "swiper/css";
 import "swiper/css/autoplay";
 import { Autoplay } from "swiper/modules";
 import { objectInfoNumber } from "@/interfaces";
+import { useEffect, useState } from "react";
+import { Button } from "@nextui-org/react";
 
 const listInfoNumber: objectInfoNumber[] = [
   {
@@ -28,43 +30,54 @@ const listInfoNumber: objectInfoNumber[] = [
 ];
 
 export default function HeroSectionTwo() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    if (listInfoNumber) {
+      setIsLoading(false);
+    }
+  }, []);
+  
   return (
-    <Swiper
-      className=" w-full h-[350px] flex justify-center items-center gap-x-14 bg-hero-sectionTwo bg-no-repeat bg-cover bg-center mt-10 "
-      spaceBetween={0}
-      slidesPerView={4}
-      modules={[Autoplay]}
-      autoplay={{ delay: 5000 }}
-      spaceBetween={0}
-      slidesPerView={1}
-      breakpoints={{
-        620: {
-          slidesPerView: 2,
-          spaceBetween: 0,
-          slidesPerGroup: 2,
-        },
-        920: {
-          slidesPerView: 3,
-          spaceBetween: 0,
-          slidesPerGroup: 3,
-        },
-        1250: {
-          slidesPerView: 4,
-          spaceBetween: 0,
-          slidesPerGroup: 4,
-        },
-      }}
-    >
-      {listInfoNumber.map((item, index) => (
-        <SwiperSlide key={index}>
-          <div className="h-full w-full flex items-center justify-center">
-            <CardInfoNumber
-              numberTitle={item.numberTitle}
-              description={item.description}
-            />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        className=" w-full h-[350px] flex justify-center items-center gap-x-14 bg-hero-sectionTwo bg-no-repeat bg-cover bg-center mt-10 "
+        modules={[Autoplay]}
+        autoplay={{ delay: 5000 }}
+        spaceBetween={0}
+        slidesPerView={1}
+        breakpoints={{
+          620: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+            slidesPerGroup: 2,
+          },
+          920: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+            slidesPerGroup: 3,
+          },
+          1250: {
+            slidesPerView: 4,
+            spaceBetween: 0,
+            slidesPerGroup: 4,
+          },
+        }}
+      >
+        {isLoading ? (
+          <Button isLoading>Cargando...</Button>
+        ) : (
+          listInfoNumber.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div className="h-full w-full flex items-center justify-center">
+                <CardInfoNumber
+                  numberTitle={item.numberTitle}
+                  description={item.description}
+                />
+              </div>
+            </SwiperSlide>
+          ))
+        )}
+      </Swiper>
+    </>
   );
 }
